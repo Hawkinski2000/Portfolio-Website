1dc4e2985fa7a7bb481ef027f3ed4ad8fb7acaa9
Kieran <hawkinski2019@gmail.com>
2025-07-08 20:52:21 -0700

    feat: store theme preference using localStorage
    Store the user's theme preference (light or dark) in localStorage so
    their preferred theme will be used when they return to the site.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
+let theme = localStorage.getItem('theme');
+
+if (!theme) {
+  localStorage.setItem('theme', 'light');
+}
+
+if (theme === 'dark') {
+  document.body.classList.add('dark-theme');
+} 
+
 // Function to toggle the theme of the website (light or dark).
 function changeTheme() {
-        document.body.classList.toggle('dark-theme');
+  document.body.classList.toggle('dark-theme');
+  
+  if (document.body.classList.contains('dark-theme')) {
+    localStorage.setItem('theme', 'dark');
+  }
+  else {
+    localStorage.setItem('theme', 'light');
+  }
 }

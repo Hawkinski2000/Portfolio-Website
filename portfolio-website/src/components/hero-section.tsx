6fff92c7b4f80dfc253de5b71f445f2e87d707cb
Kieran <hawkinski2019@gmail.com>
2026-03-23 23:53:43 -0700

    fix: Prevent hero text from shifting up
    Prevent the hero text from shifting up when a new line is added during
    the TextType animation.

--- portfolio-website/src/components/hero-section.tsx
+++ portfolio-website/src/components/hero-section.tsx
@@ -4,12 +4,14 @@ export default function HeroSection() {
   return (
     <section className="bg-background sticky top-0 flex h-screen w-full items-center justify-center">
       <div className="w-full max-w-2xl sm:max-w-4xl">
-        <TextType
-          className="max-w-2xl px-4 text-left text-6xl font-bold sm:max-w-4xl sm:text-9xl"
-          cursorCharacter="|"
-          text={['Kieran\nHawkins']}
-          loop={false}
-        />
+        <div className="h-48 sm:h-80">
+          <TextType
+            className="max-w-2xl px-4 text-left text-6xl font-bold sm:max-w-4xl sm:text-9xl"
+            cursorCharacter="|"
+            text={['Kieran\nHawkins']}
+            loop={false}
+          />
+        </div>
       </div>
     </section>
   )
